c85c83d8078da9fbaecf5cfe004d55bd65e0c125
Dzima-G <dgukalin@gmail.com>
2023-07-09 09:55:52 +0300

    fixed data generation

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from terminaltables import AsciiTable
 
 
 def predict_salary(salary_from, salary_to):
-    if salary_from is None or not salary_from:
+    if not salary_from:
         salary_from = None
-    if salary_to is None or not salary_to:
+    if not salary_to:
         salary_to = None
-    if salary_from is not None and salary_to is not None:
+    if salary_from and salary_to:
         income_average = (int(salary_from) + int(salary_to)) / 2
         return income_average
-    if salary_from is not None:
+    if salary_from:
         income_average = int(salary_from) * 1.2
         return income_average
-    if salary_to is not None:
+    if salary_to:
         income_average = int(salary_to) * 0.8
         return income_average
     return None
@@ -83,20 +83,22 @@ def get_output_table(data, title):
 
 
 def get_average_salary(response_vacancy):
-    salary_statistics = {"vacancies_found": None, "vacancies_processed": None, "average_salary": None}
     selected_vacancies, number_vacancies = response_vacancies
     if not number_vacancies:
-        salary_statistics["vacancies_found"] = 0
-        salary_statistics["vacancies_processed"] = 0
-        salary_statistics["average_salary"] = 0
+        salary_statistics = {"vacancies_found": 0,
+                             "vacancies_processed": 0,
+                             "average_salary": 0
+                             }
         return salary_statistics
-    salary_statistics["vacancies_found"] = number_vacancies
     processed_data = [x[2] for x in selected_vacancies if x[2] is not None]
-    salary_statistics["vacancies_processed"] = len(processed_data)
     if len(processed_data):
-        salary_statistics["average_salary"] = int(sum(processed_data) / len(processed_data))
+        average_salary = int(sum(processed_data) / len(processed_data))
     else:
-        salary_statistics["average_salary"] = int(sum(processed_data))
+        average_salary = int(sum(processed_data))
+    salary_statistics = {"vacancies_found": number_vacancies,
+                         "vacancies_processed": len(processed_data),
+                         "average_salary": average_salary,
+                         }
     return salary_statistics
 
 
@@ -106,7 +108,6 @@ if __name__ == "__main__":
     vacancies = ["Программист Python", "Программист Java", "Программист JavaScript", "Программист Ruby",
                  "Программист PHP", "Программист C++", "Программист C#", "Программист C", "Программист Go",
                  "Программист Swift"]
-
     table_output_vacancies = dict.fromkeys(vacancies)
 
     for vacancy in vacancies:
@@ -128,5 +129,4 @@ if __name__ == "__main__":
         response_vacancies = (response_vacancies, len(response_vacancies))
         vacancy_statistics = get_average_salary(response_vacancies)
         table_output_vacancies[vacancy] = vacancy_statistics
-
     print(get_output_table(table_output_vacancies, 'SuperJob Moscow'))
