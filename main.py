253643a4e5d6caea5baa7061fa9dcf081cb381de
Dzima-G <dgukalin@gmail.com>
2023-07-09 08:41:59 +0300

    function exit fixed

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from terminaltables import AsciiTable
 
 
 def predict_salary(salary_from, salary_to):
-    if salary_from is None or salary_from <= 0:
+    if salary_from is None or not salary_from:
         salary_from = None
-    if salary_to is None or salary_to <= 0:
+    if salary_to is None or not salary_to:
         salary_to = None
     if salary_from is not None and salary_to is not None:
         income_average = (int(salary_from) + int(salary_to)) / 2
@@ -22,47 +22,50 @@ def predict_salary(salary_from, salary_to):
 
 
 def predict_rub_salary_hh(vacancy):
-    profession = []
+    professions = []
+    city = 1
     found = 0
     page = 0
     while True:
         page += 1
-        payload = {"text": {vacancy}, "area": "1", "per_page": "50", "page": {page}}
+        payload = {"text": {vacancy}, "area": city, "per_page": "50", "page": {page}}
         response = requests.get("https://api.hh.ru/vacancies", params=payload)
-        data = response.json()
-        if "items" not in data:
+        vacancy_data = response.json()
+        if "items" not in vacancy_data:
             break
         if page == 1:
-            found = data["found"]
-        for item_vacancy in (data["items"]):
+            found = vacancy_data["found"]
+        for item_vacancy in (vacancy_data["items"]):
             if item_vacancy["salary"] is not None and item_vacancy["salary"]["currency"] in "RUR":
                 predict_salary(item_vacancy["salary"]["from"], item_vacancy["salary"]["to"])
-                profession.append([
+                professions.append([
                     f"{item_vacancy['name']}",
                     f"{item_vacancy['area']['name']}",
                     predict_salary(item_vacancy["salary"]["from"], item_vacancy["salary"]["to"])
                 ])
-    return profession, found
+    return professions, found
 
 
 def predict_rub_salary_sj(vacancy):
     headers_sj = {"X-Api-App-Id": sj_secret_key}
+    city = 4
+    industries_sections = 48
     page = 0
-    profession = []
+    professions = []
     while True:
-        payload = {"keyword": vacancy, "town": "4", "catalogues": "48", "count": "50", "page": page}
+        payload = {"keyword": vacancy, "town": city, "catalogues": industries_sections, "count": "50", "page": page}
         response = requests.get("https://api.superjob.ru/2.0/vacancies/", headers=headers_sj, params=payload)
         response.raise_for_status()
         vacancy_data = response.json()
-        if len(vacancy_data['objects']) == 0:
+        if not len(vacancy_data['objects']):
             break
         for item_vacancy in vacancy_data['objects']:
             if item_vacancy['currency'] == "rub":
-                profession.append(
+                professions.append(
                     [f"{item_vacancy['profession']}", f"{item_vacancy['town']['title']}",
                      predict_salary(item_vacancy['payment_from'], item_vacancy['payment_to'])])
         page += 1
-    return profession, len(profession)
+    return professions
 
 
 def get_output_table(data, title):
@@ -79,46 +82,49 @@ def get_output_table(data, title):
     return table.table
 
 
-def get_average_salary(list_vacancies, source):
-    vacancies = dict.fromkeys(list_vacancies)
-    for vacancy in list_vacancies:
-        salary_statistics = {"vacancies_found": None, "vacancies_processed": None, "average_salary": None}
-        if source == "sj":
-            income_response = predict_rub_salary_sj(vacancy)
-        else:
-            income_response = predict_rub_salary_hh(vacancy)
-        if income_response[1] == 0:
-            salary_statistics["vacancies_found"] = 0
-            salary_statistics["vacancies_processed"] = 0
-            salary_statistics["average_salary"] = 0
-            vacancies[vacancy] = salary_statistics
-            continue
-        salary_statistics["vacancies_found"] = income_response[1]
-        processed_data = [x[2] for x in income_response[0] if x[2] is not None]
-        salary_statistics["vacancies_processed"] = len(processed_data)
-        if len(processed_data):
-            salary_statistics["average_salary"] = int(sum(processed_data) / len(processed_data))
-            vacancies[vacancy] = salary_statistics
-        else:
-            salary_statistics["average_salary"] = int(sum(processed_data))
-            vacancies[vacancy] = salary_statistics
-    return vacancies
+def get_average_salary(response_vacancy):
+    salary_statistics = {"vacancies_found": None, "vacancies_processed": None, "average_salary": None}
+    if not response_vacancy[1]:
+        salary_statistics["vacancies_found"] = 0
+        salary_statistics["vacancies_processed"] = 0
+        salary_statistics["average_salary"] = 0
+        return salary_statistics
+    salary_statistics["vacancies_found"] = response_vacancy[1]
+    processed_data = [x[2] for x in response_vacancy[0] if x[2] is not None]
+    salary_statistics["vacancies_processed"] = len(processed_data)
+    if len(processed_data):
+        salary_statistics["average_salary"] = int(sum(processed_data) / len(processed_data))
+    else:
+        salary_statistics["average_salary"] = int(sum(processed_data))
+    return salary_statistics
 
 
 if __name__ == "__main__":
     load_dotenv()
     sj_secret_key = os.environ['SUPER_JOB_SECRET_KEY']
-    vacancies = ["Программист Python", "Программист Java"]
+    vacancies = ["Программист Python", "Программист Java", "Программист JavaScript", "Программист Ruby",
+                 "Программист PHP", "Программист C++", "Программист C#", "Программист C", "Программист Go",
+                 "Программист Swift"]
+    table_output_vacancies = dict.fromkeys(vacancies)
 
-    try:
-        hh_vacancies_data = get_average_salary(vacancies, "hh")
-    except requests.exceptions.HTTPError:
-        print('Ошибка! Некорректная ссылка')
+    for vacancy in vacancies:
+        try:
+            response_vacancies = predict_rub_salary_hh(vacancy)
+        except requests.exceptions.HTTPError:
+            print('Ошибка! Некорректная ссылка')
 
-    print(get_output_table(hh_vacancies_data, 'HeadHunter Moscow'))
+        vacancy_statistics = get_average_salary(response_vacancies)
+        table_output_vacancies[vacancy] = vacancy_statistics
+    print(get_output_table(table_output_vacancies, 'HeadHunter Moscow'))
 
-    try:
-        sj_vacancies_data = get_average_salary(vacancies, "sj")
-    except requests.exceptions.HTTPError:
-        print('Ошибка! Некорректная ссылка')
-    print(get_output_table(sj_vacancies_data, 'SuperJob Moscow'))
+    for vacancy in vacancies:
+        try:
+            response_vacancies = predict_rub_salary_sj(vacancy)
+        except requests.exceptions.HTTPError:
+            print('Ошибка! Некорректная ссылка')
+
+        response_vacancies = (response_vacancies, len(response_vacancies))
+        vacancy_statistics = get_average_salary(response_vacancies)
+        table_output_vacancies[vacancy] = vacancy_statistics
+
+    print(get_output_table(table_output_vacancies, 'SuperJob Moscow'))
